490af6473c77082094be5826c125e63f249be98f
impovich <impovich2@gmail.com>
2022-09-26 21:14:12 +0200

    add logback config

---
+++ sensor-stats-ingester-integrations/sensor-stats-ingester-integration-http/src/main/kotlin/com/fourofour/home/sensor/stats/ingester/integration/http/config/BleBoxIntegrationConfig.kt
@@ -0,0 +1,7 @@
+package com.fourofour.home.sensor.stats.ingester.integration.http.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BleBoxIntegrationConfig {
+}
